1f02980dc2a28081190a483f9d6086070033720c
KanashiiDev <18560400+KanashiiDev@users.noreply.github.com>
2026-08-09 02:12:53 +0300

    feat(scripts): add Monochrome activity

--- activities/M/monochrome.js
+++ activities/M/monochrome.js
@@ -6,7 +6,7 @@ registerParser({
   title: "Monochrome",
   version: "1.0.2",
   description: "",
-  lastUpdated: "1786230165831",
+  lastUpdated: "1786230230653",
   mode: "listen",
   watchAutoDetect: "disable",
   homepage: "",
@@ -20,7 +20,7 @@ registerParser({
     let songUrl = "";
     let timePassed = getText('#current-time');
     let duration = getText('#total-duration');
-    let buttons = [{ link: "test", text: "test" }, { link: "", text: "", }];
+    let buttons = [{ link: "", text: "test" }, { link: "", text: "", }];
     let isPlaying = querySelectorDeep('div.buttons > .play-pause-btn path[d*=M6]');
   },
 });
