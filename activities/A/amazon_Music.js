a6d1013eb65816b269b56c496cc9d8c289b716f9
wolfboyvr! <supermaxcooldog@gmail.com>
2026-09-01 20:33:52 +0100

    fix(scripts): update Amazon Music to v2.0.0

---
+++ activities/A/amazon_Music.js
@@ -0,0 +1,27 @@
+registerParser({
+  id: "kanashiidev_music.amazon.com_Lio",
+  domain: "music.amazon.com",
+  authors: "kanashiiDev",
+  authorsLinks: "https://github.com/KanashiiDev",
+  title: "Amazon Music",
+  version: "2.0.0",
+  description: "On-demand music streaming service with songs, playlists, and integrated podcasts.",
+  lastUpdated: "1788291214780",
+  mode: "listen",
+  watchAutoDetect: "disable",
+  homepage: "",
+  category: "platform",
+  urlPatterns: [/.*/],
+  fn: async function () {
+    const main = document.querySelector("music-horizontal-item")?.shadowRoot;
+      const title = getText("music-link", { root: main });
+      const artist = getText("music-link[kind='secondary']", { root: main });
+      const image = getText("music-image", { root: main, attr: "src", transform: (v) => v.replace(/\.jpg$/, "._SX160_SY160_.jpg") });
+      const songUrl = main?.querySelector("music-link a")?.href || "https://www.music.amazon.com/";
+      const times = document.querySelector("#progress-container")?.getAttribute("aria-valuetext").split("/");
+      const timePassed = times ? times[0] : "";
+      const duration = times ? times[1] : "";
+      const isPlaying = Boolean(main?.querySelector("button[aria-label='Pause']"));
+      const source = "Amazon Music";
+  },
+});
